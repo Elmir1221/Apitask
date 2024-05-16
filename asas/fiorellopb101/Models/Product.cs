f8e95abd35c3d0a93b81b01a38794bd8d56177b2
1907elnur <MY_NAME@example.com>
2024-05-16 08:54:28 +0400

    task

---
+++ asas/fiorellopb101/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace fiorellopb101.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public Category category { get; set; }
+        public ICollection<ProductImage> ProductImages { get; set; }
+
+    }
+}
---
+++ asas/fiorellopb101/Servioces/ProductServices.cs
@@ -0,0 +1,21 @@
+﻿using fiorellopb101.Data;
+using fiorellopb101.Models;
+using fiorellopb101.Servioces.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace fiorellopb101.Servioces
+{
+    public class ProductServices : IProductService
+    {
+        private readonly AppDbContext _context;
+
+        public ProductServices(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Product.Include(m => m.ProductImages).ToListAsync();
+        }
+    }
+}
